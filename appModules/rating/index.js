92f2d6ff8425a18a0841cd7264f01c8d15cf5fc4
MishaBozhevskiy <misha.bozhev@gmail.com>
2024-04-14 23:23:43 +0300

    feat: add project files

---
+++ appModules/rating/index.js
@@ -0,0 +1,5 @@
+const config = require("./config");
+const  makeRatingFile  = require("./rating-file");
+const calculations  = require("./calculations");
+
+module.exports = { config, makeRatingFile, calculations };
