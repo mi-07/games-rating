4d1c6b099b3f24a3514b84e7d00a8d2c1ec39adc
MishaBozhevskiy <misha.bozhev@gmail.com>
2024-04-27 13:27:56 +0300

    feat: add routers, simple games data json, add read/write files method and add 'express' framework

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-const http = require("http");
-const path = require("path");
-const { mimeTypes } = require("./appModules/http-utils/");
-const { mainRouteController, defaultRouteController, gameRouteController, voteRouteController } = require("./controllers/index");
-// ...импорты и настройки
+// const http = require("http");
+// const path = require("path");
+// const { mimeTypes } = require("./appModules/http-utils/");
+// const { mainRouteController, defaultRouteController, gameRouteController, voteRouteController } = require("./controllers/index");
+// // ...импорты и настройки
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  switch (url) {
-    case "/":
-      mainRouteController(res, "/index.html", ".html");
-      break;
-    case "/game":
-      gameRouteController(res);
-      break;
-    case "/vote":
-      voteRouteController(req, res);
-      break;
-    default:
-      defaultRouteController(res, url);
-  }
-});
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   switch (url) {
+//     case "/":
+//       mainRouteController(res, "/index.html", ".html");
+//       break;
+//     case "/game":
+//       gameRouteController(res);
+//       break;
+//     case "/vote":
+//       voteRouteController(req, res);
+//       break;
+//     default:
+//       defaultRouteController(res, url);
+//   }
+// });
 
-server.listen(3005); // ...порт
+// server.listen(3005); // ...порт
+
+
+// app.js 
+const express = require('express');
+
+// Импортируем роутеры
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games'); 
+
+const PORT = 3000;
+const app = express();
+
+// Запускаем
+app.use(mainRoute, gamesRouter); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
+})
